4307d667bfaf776b18b210fbbfb6abf637ca00e9
yunusemre12500 <131378214+yunusemre12500@users.noreply.github.com>
2025-05-27 21:42:54 +0300

    chore: add development shell for dotnet

---
+++ dotnet/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = ".NET Development Environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+  };
+
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            dotnet-sdk
+            nuget
+          ];
+
+          shellHook = "export PATH=~/.cargo/bin:$PATH";
+
+          CC = "clang";
+          CXX = "clang++";
+
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          RUSTC_WRAPPER = "sccache";
+        };
+      });
+}
