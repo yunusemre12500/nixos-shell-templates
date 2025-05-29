243e096dff5ee5c5ebe1fc1288ab1db7366bfcdc
yunusemre12500 <131378214+yunusemre12500@users.noreply.github.com>
2025-05-29 14:33:32 +0300

    chore: add development environment for C

---
+++ c/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "C Development Environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+  };
+
+  outputs =
+    { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            clang
+          ];
+
+          shellHook = ''
+            clang -v
+          '';
+        };
+      }
+    );
+}
